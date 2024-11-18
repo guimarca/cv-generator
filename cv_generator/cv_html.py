a24ab21bd8acefe37a721277e124d1e657714ee6
fcojavierMT <fcojavierdev@gmail.com>
2024-11-18 10:01:31 +0100

    feat(lang): added multilingual support

--- cv_generator/cv_html.py
+++ cv_generator/cv_html.py
@@ -1,4 +1,5 @@
 import os.path
+import yaml
 
 from jinja2 import Environment, select_autoescape, FileSystemLoader
 
@@ -13,10 +14,16 @@ def get_output_name(cv: dict) -> str:
     return f"CV-{cv['general']['fullName'].split(' ')[0]}"
 
 
-def generate_html(cv: dict, full=False):
+def load_transactions(language):    
+    with open(os.path.join(f'{ASSETS_PATH}/languages', f'{language}.yml'), 'r') as file:
+        return yaml.safe_load(file)
+
+
+def generate_html(cv: dict, full=False, lang='en'):
+    translations = load_transactions(lang)
     t = env.get_template("template.html")
     output_path = f"{os.path.join(OUTPUT_PATH, get_output_name(cv))}.html"
-    t.stream(**cv, fullCV=full).dump(output_path)
+    t.stream(**cv, fullCV=full, translations=translations).dump(output_path)
     print(
         f"html done! file://{os.path.join(os.path.abspath(os.getcwd()), output_path)}\n"
     )
